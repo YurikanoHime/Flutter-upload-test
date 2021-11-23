50ec49969b3012546a4e5933bf18a3a6ad8ba5e2
shaohuakdi@gmail.com <shaohuakdi@gmail.com>
2021-11-22 21:39:27 -0500

    commit test

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:css_colors/css_colors.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Welcome to Flutter'),
+        ),
+        body: Center(
+          //child: Text('Hello World'),
+          child: Text(wordPair.asPascalCase),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
